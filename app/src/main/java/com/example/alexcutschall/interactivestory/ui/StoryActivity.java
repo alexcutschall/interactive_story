fc6c796e4a1a94f3ea94ca67c411770503c0caa7
alexcutschall <alexander.cutschall@gmail.com>
2018-07-05 07:57:21 -0600

    adding in the first parts

---
+++ app/src/main/java/com/example/alexcutschall/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,26 @@
+package com.example.alexcutschall.interactivestory.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.alexcutschall.interactivestory.R;
+
+public class StoryActivity extends AppCompatActivity {
+
+    public final static String TAG = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+        if (name == "null" || name.isEmpty()) {
+            name = "Friend";
+        }
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+    }
+}
